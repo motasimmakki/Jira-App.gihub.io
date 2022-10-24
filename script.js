2621d2b6fbce3b9a09971cc97564fc653d4bbd8a
motasimmakki <makkimotasim@gmail.com>
2022-10-24 17:34:39 +0530

    :technologist: feat: Using local storage to store previous tickets

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const modalCont = document.querySelector(".MainArea");
 const textArea = document.querySelector(".TypingArea");
 const mainCont = document.querySelector(".main-cont");
 const allPriorityColors = document.querySelectorAll(".PriorityColor");
+const addTodoCont = document.querySelectorAll(".AddTodo>*");
+let uid = new ShortUniqueId();
+let ticketsArr = [];
 
 // Colors array.
 const colors = ["Pink", "Green", "Blue", "Black"];
@@ -55,20 +58,37 @@ modalCont.addEventListener("keydown", function(event) {
    }
 });
 
-function createTicket(ticketColor, data) {
-   // Generating short unique id npm package.
-   let id = new ShortUniqueId();
+function createTicket(ticketColor, data, ticketID) {
+   // Generating short unique id using npm package.
+   let id = ticketID || uid();
    let ticketCont = document.createElement("div");
    ticketCont.setAttribute("class", "ticket-cont");
    ticketCont.innerHTML = `
       <div class="ticket-color ${ticketColor}"></div>
-      <div class="ticket-id">#${id()}</div>
+      <div class="ticket-id">#${id}</div>
       <div class="task-area">${data}</div>
       <div class="ticket-lock">
          <i class="fa-solid fa-lock"></i>
       </div>
    `;
    mainCont.appendChild(ticketCont);
+   
+   // If ticket is being generated for the first time, save it in local storage.
+   if(!ticketID) {
+      ticketsArr.push({
+         ticketID: id,
+         ticketColor,
+         ticketTask: data
+      });
+      localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+   }
+}
+// Getting data from local storage for re-rendering of tickets.
+if(localStorage.getItem("tickets")) {
+   ticketsArr = JSON.parse(localStorage.getItem("tickets"));
+   ticketsArr.forEach(ticketObj => {
+      createTicket(ticketObj.ticketColor, ticketObj.ticketTask, ticketObj.ticketID);
+   });
 }
 
 allPriorityColors.forEach(colorElement => {
@@ -78,5 +98,13 @@ allPriorityColors.forEach(colorElement => {
       });
       colorElement.classList.add("active");
       modalPriorityColor = colorElement.classList[0];
+   });
+});
+
+// Getting ticket on the basis of ticket color.
+addTodoCont.forEach(toolBoxColor => {
+   toolBoxColor.addEventListener("click", function() {
+      let currColor = this.classList[0];
+      console.log(currColor);
    });
 });
